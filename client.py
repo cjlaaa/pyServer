911b9c21a62f929a99a1e9cb2c6a82dc4752acf9
cjlaaa <cjlaaa@gmail.com>
2023-07-18 08:03:12 +0800

    1.自定义socket协议包头,使用protobuf作为包体进行拆包,组包 2.对错误消息抛异常,并输出日志 3.使用标准库的logging日志系统 4.客户端登出简单处理 5.目前为每个客户端创建一个线程阻塞收取消息,未来会优化为异步编程,考虑使用多协程技术 6.未来准备先做成无状态服务器,数据存redis

---
+++ client.py
@@ -0,0 +1,59 @@
+import socket
+import struct
+
+from msg import UserMsg_pb2
+
+
+def client_program():
+    # 服务器的IP地址和端口号
+    server_ip = '127.0.0.1'
+    server_port = 12345
+    for i in range(1):
+        # 创建一个套接字对象
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # 连接到服务器
+        client_socket.connect((server_ip, server_port))
+
+        # 发送数据到服务器
+        # 创建一个空的 protobuf 对象
+        message = UserMsg_pb2.C2SLogin()
+        # 设置 protobuf 对象的字段
+        message.Account = "cjlaaa"+str(i)
+
+        # 序列化 protobuf 对象
+        proto_msg = message.SerializeToString()
+        # 获取包体长度
+        message_length = len(proto_msg)
+
+        # 定义包头格式
+        header_format = 'ii'  # 消息类型和消息长度都是4字节的整数,后面为了测试又加了一个整数
+        message_type = 1
+        # 将消息类型和包体长度打包成字节对象
+        header = struct.pack(header_format, message_type, message_length)
+
+        # 拼接包头和包体
+        packet = header + proto_msg
+
+        client_socket.send(packet)
+
+        # 接收服务器返回的数据
+        data = client_socket.recv(10240)
+        print('从服务器收到的消息：', data)
+
+        unpacked_header = struct.unpack(header_format, data[:8])
+        received_message_type = unpacked_header[0]
+        received_message_length = unpacked_header[1]
+        received_message = data[8:]
+
+        received_proto_obj = UserMsg_pb2.S2CLogin()
+        received_proto_obj.ParseFromString(received_message)
+        print(received_message_type, received_message_length, received_proto_obj)
+
+
+        # 关闭套接字连接
+        client_socket.close()
+
+
+if __name__ == '__main__':
+    client_program()
---
+++ server.py
@@ -0,0 +1,117 @@
+import logging
+import socket
+import struct
+import threading
+from msg import UserMsg_pb2
+import traceback
+
+# 定义服务器地址和端口
+SERVER_HOST = 'localhost'
+SERVER_PORT = 12345
+
+# 定义客户端列表
+clients = []
+
+# 定义锁，用于保护clients列表的并发访问
+lock = threading.Lock()
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s[%(levelname)s]%(message)s(%(filename)s:%(lineno)d)')
+logging.basicConfig(level=logging.ERROR, format='%(asctime)s[%(levelname)s]%(message)s(%(filename)s:%(lineno)d)')
+
+# 处理客户端请求的线程函数
+global_count = 0
+
+
+def handle_client(client_socket):
+    global global_count
+    while True:
+        global_count = global_count + 1
+        logging.debug(f"global_count:{global_count}")
+        try:
+            header = client_socket.recv(8)
+            if not header:
+                logging.debug(f"收到错误的头{header}")
+                break
+            header_format = 'ii'  # 消息类型和消息长度都是4字节的整数
+            unpacked_header = struct.unpack(header_format, header)
+            received_message_type = unpacked_header[0]
+            received_message_length = unpacked_header[1]
+            received_message = client_socket.recv(received_message_length)
+
+            # 处理登录请求
+            if received_message_type == 1:
+                # 解析protobuf消息
+                msg = UserMsg_pb2.C2SLogin()
+                msg.ParseFromString(received_message)
+
+                # 获取用户名
+                account = msg.Account
+                logging.debug(f"用户{account}已登录")
+
+                # 添加客户端到列表
+                with lock:
+                    clients.append(client_socket)
+
+                # 发送登录成功响应
+                response_message = UserMsg_pb2.S2CLogin()
+                response_message.Res = 999
+                response_message.Message = "Hey you!" + account
+                response_message.PlayerId = 123456
+
+                # 序列化 protobuf 对象
+                response_message_proto = response_message.SerializeToString()
+                # 获取包体长度
+                response_message_proto_length = len(response_message_proto)
+
+                header_format = 'ii'  # 消息类型和消息长度都是4字节的整数,后面为了测试又加了一个整数
+                message_type = 7890
+                response_message_header = struct.pack(header_format, message_type, response_message_proto_length)
+                response_packet = response_message_header + response_message_proto
+                client_socket.send(response_packet)
+
+            # 处理其他请求
+            elif received_message_type == 2:
+                # 解析protobuf消息
+                # msg = UserMsg_pb2.C2SLogin()
+                # msg.ParseFromString(received_message)
+
+                # # 广播消息给所有客户端
+                # with lock:
+                #     for client in clients:
+                #         client.send(message.encode())
+                pass
+            else:
+                raise Exception("收到错误的包", received_message_type, received_message_length, received_message)
+        except Exception as e:
+            logging.debug(f"Exception: {e}")
+            traceback.print_exc()
+            break
+
+    # 客户端断开连接，从列表中移除
+    with lock:
+        ip_address, port = client_socket.getpeername()
+        try:
+            clients.remove(client_socket)
+            logging.debug(f"{ip_address}:{port}已登出")
+
+        except ValueError:
+            logging.debug(f"{ip_address}:{port}非正常登出")
+
+    client_socket.close()
+
+
+# 创建服务器socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((SERVER_HOST, SERVER_PORT))
+server_socket.listen(5)
+
+logging.debug(f"Server listening on {SERVER_HOST}:{SERVER_PORT}")
+
+while True:
+    # 接受客户端连接
+    client_socket, client_address = server_socket.accept()
+    logging.debug(f"New connection from {client_address[0]}:{client_address[1]}")
+
+    # 创建新的线程处理客户端请求
+    client_thread = threading.Thread(target=handle_client, args=(client_socket,))
+    client_thread.start()
